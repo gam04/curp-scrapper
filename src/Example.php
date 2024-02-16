ca204a565af4c6071b1de7208263d1481df470cf
Antonio Gamboa <antgam95@gmail.com>
2024-02-16 15:09:50 -0600

    chore: initialize project using ramsey/php-library-starter-kit

---
+++ src/Example.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * This file is part of gam/curp-scrapper
+ *
+ * @copyright Copyright (c) Antonio Gamboa <antgam95@gmail.com>. All rights reserved.
+ */
+
+declare(strict_types=1);
+
+namespace Gam\CurpScrapper;
+
+/**
+ * An example class to act as a starting point for developing your library
+ */
+class Example
+{
+    /**
+     * Returns a greeting statement using the provided name
+     */
+    public function greet(string $name = 'World'): string
+    {
+        return "Hello, {$name}!";
+    }
+}
